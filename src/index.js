15f46aa3ce9b30cc7175d67ea0dabaae55e5588c
Marina-Poprotskaya <60131955+Marina-Poprotskaya@users.noreply.github.com>
2020-03-17 18:16:19 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = [];
+  for (let k = 0; k < bracketsConfig.length;k++) {
+       brackets.push(bracketsConfig[k].join(''));    
+  }
+ for (let i = 0; i < brackets.length;) {
+   if (str.includes(brackets[i])) {
+     str = str.replace(brackets[i], '');
+     i=0;
+   } else i++
+ }
+
+ let result;
+ str.length > 0 ? result = false: result = true;
+ return result;
 }
+
+
+
